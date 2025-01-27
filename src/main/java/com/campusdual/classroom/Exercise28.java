3542e8a63584b228e27f2438bae1a13f83ad4666
nievesfa <nievesfacorro@gmail.com>
2025-01-27 18:24:34 +0100

    test ok,

--- src/main/java/com/campusdual/classroom/Exercise28.java
+++ src/main/java/com/campusdual/classroom/Exercise28.java
@@ -3,5 +3,8 @@ package com.campusdual.classroom;
 public class Exercise28 {
     public static void main(String[] args) {
 
+        int diviendo = 5, divisor = 0;
+        int res = diviendo/divisor;
+        System.out.println(res);
     }
 }
